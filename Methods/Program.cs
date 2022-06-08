512c81bdb8a2d2dd1beecc32ab9079b4c58a0774
schavis2022 <schavis2022>
2022-06-08 11:22:01 -0400

    Funny Method

---
+++ Methods/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("What is your name");
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
+            Console.WriteLine("What is you favorite color");
+            string color = Console.ReadLine();
+            Console.WriteLine(color);
+            Console.WriteLine("Favorite Animal");
+            string animal = Console.ReadLine();
+            Console.WriteLine(animal);
+            Console.WriteLine("Favorite Band");
+            string band = Console.ReadLine();
+            Console.WriteLine(band);
+            
+            
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
