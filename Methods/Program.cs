ee83bb34bdf6c8084ea8e45afe8afe0159a23c79
schavis2022 <schavis2022>
2022-06-09 15:22:43 -0400

    Method with Funny Story

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -7,81 +7,71 @@ namespace Methods
         static void Main(string[] args)
 
         {
-            Console.WriteLine("What is your name");
+            Console.WriteLine("What your name");
             string name = Console.ReadLine();
             Console.WriteLine(name);
+
             Console.WriteLine("What is you favorite color");
             string color = Console.ReadLine();
             Console.WriteLine(color);
-            Console.WriteLine("Favorite Animal");
+
+            Console.WriteLine("What is favorite animal");
             string animal = Console.ReadLine();
             Console.WriteLine(animal);
-            Console.WriteLine("Favorite Band");
+
+            Console.WriteLine("Whats you dogs name");
+            string dogName = Console.ReadLine();
+            Console.WriteLine(dogName);
+
+            Console.WriteLine("What is you favorite band?");
             string band = Console.ReadLine();
             Console.WriteLine(band);
-            
-            
-         
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            Console.WriteLine($"There handsome man named {name}, his eyes were my favorite {color}.");
+            Console.WriteLine($"He had a {animal} called {dogName}, he was also {color}.");
+            Console.WriteLine($"The handsome man {name} and his dog {dogName} loved listening to his" +
+            $"his favorite {band}");
 
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
